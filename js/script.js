12e20bdd0017bfbb06505d7f4ae04994e5f9f434
ilario-moreni <ilario2001.mor@gmail.com>
2022-11-28 17:58:48 +0100

    first processing style

--- js/script.js
+++ js/script.js
@@ -1,30 +1,35 @@
-/* 1 */
 
-let numOfKm = prompt('Inserisci i kilometri da percorrere:');
+let numOfKm;
+
+const submit = document.getElementById("submit");
+
+submit.addEventListener("click", function(){
+    numOfKm = document.getElementById('numOfKm').value;
+    document.getElementById('output_km').innerHTML = numOfKm;
+    console.log(numOfKm)
+});
+
+
 
 numOfKm = parseFloat(numOfKm);
 
-console.log(`${numOfKm}: numero di kilometri`)
+let fullName = document.getElementById('name');
 
-/* 2 */
 
-let age = prompt('Inserisci la tua età:');
+/* let age = prompt('Inserisci la tua età:');
 
 age = parseFloat(age);
 
 console.log(`${age}: età del passeggero`);
 
-/* 3 */
 
 let fullPrice = 0.21 * numOfKm;
 
 console.log(`${fullPrice}: prezzo pieno`);
 
-/* 4 */
 
 let youthSale, seniorSale, finalPrice;
 
-/* il sottraendo (20 e 40) corrisponde allo sconto applicato */
 youthSale = (100 - 20) / 100;
 console.log(`${youthSale}: sconto per minorenni`);
 
@@ -47,12 +52,11 @@ console.log(`${finalPrice}: prezzo scontato non arrotondato`);
 finalPrice = finalPrice.toFixed(2);
 
 
-/* 5 */
 
 let finalSentece = `Il prezzo del biglietto è di ${finalPrice}€`;
 
 console.log(finalSentece)
 
-/* 6 (bonus) */
 
 document.getElementById('ticketPrice').innerHTML = finalSentece;
+ */
